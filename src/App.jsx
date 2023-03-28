4efbc37e5a67c40372de1a842642be5596fc233c
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-28 17:21:00 +0600

    dynamic-data

--- src/App.jsx
+++ src/App.jsx
@@ -13,28 +13,28 @@ function App() {
 
 	return (
 		<div className='App'>
-			<div>
-				<a href='https://vitejs.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://reactjs.org' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h2 style={newStyle}>Hi i am touhidcodes</h2>
-			<h2 className="new">Hi i am touhidcodes</h2>
-			<h1>Bite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
-			</p>
+			<Person name='Sakib al Hasan' profession='Cricket'></Person>
+			<Person name='Sakib Khan' profession='Actor'></Person>
+			<Friend name='Sakib' profession='Student'></Friend>
+			<Friend name='Rahim' profession='Freelancer'></Friend>
+		</div>
+	);
+}
+
+function Person(props) {
+	return (
+		<div className='person'>
+			<h1>Name: {props.name}</h1>
+			<p>Profession: {props.profession}</p>
+		</div>
+	);
+}
+
+function Friend(props) {
+	return (
+		<div className='person'>
+			<h1>Friend: {props.name}</h1>
+			<p>Profession: {props.profession}</p>
 		</div>
 	);
 }
